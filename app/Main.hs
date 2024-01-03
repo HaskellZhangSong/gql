e3d269747ad6a9c80b5c60cd437a0c69654767db
songzh <haskell.songzh@hotmail.com>
2024-01-03 16:27:18 +0800

    remove edges with out line null

--- app/Main.hs
+++ app/Main.hs
@@ -11,22 +11,23 @@ import Text.Pretty.Simple (pPrint)
 import System.Exit
 import Prettyprinter
 import Data.Function
+import Data.List
 
 hfile :: GQL -> String
 hfile graph = let paths = gql2Paths graph
                   index_table = getAllTable graph
                   p = path graph
-                  tis = pathToTableInfo p
+                  tes = tableAdjacent p []
                   ws = map (encode index_table) paths
                 in unlines [
     "#pragma once",
     "#ifndef GQL_ENCODE_H",
     "#define GQL_ENCODE_H",
     "#include <stdint.h>",
-    printf "#define EDGE_INDEX_LEN %d" (numOfEdges (path graph)),
+    printf "#define EDGE_INDEX_LEN %d" (length $ nub $ concatMap snd tes),
     "extern uint32_t edgeIndexArr[EDGE_INDEX_LEN];",
     printf "#define TABLE_INDEX_LEN %d" (length index_table),
-    "extern uint32_t tableIndexArr[TABLE_INDEX_LEN] ;",
+    "extern uint32_t tableIndexArr[TABLE_INDEX_LEN];",
     printf "#define PATH_CODES_LEN %d" (length paths) , 
     "extern uint32_t pathCodes[PATH_CODES_LEN];",
     "#endif"
@@ -35,12 +36,13 @@ hfile graph = let paths = gql2Paths graph
 table_info_hfile :: GQL -> String
 table_info_hfile graph = let p = path graph
                              tis = pathToTableInfo p
+                             tes = tableAdjacent p []
                         in unlines
     ["#pragma once",
      "#ifndef VERTEX_EDGES_INFO_H",
      "#define VERTEX_EDGES_INFO_H",
      "#include <stdint.h>",
-     printf "#define EDGE_IDS_SIZE %d" (numOfEdges p),
+     printf "#define EDGE_IDS_SIZE %d" (length $ nub $ concatMap snd tes),
      idHashPair,
      vertexEdgesInfo,
      "extern IdHashPair g_edge_id_hash_pairs[EDGE_IDS_SIZE];",
@@ -52,12 +54,13 @@ cfile :: GQL -> String
 cfile graph = let paths = gql2Paths graph
                   p = path graph
                   tis = pathToTableInfo p
+                  tes = tableAdjacent p []
                   index_table = getAllTable graph
                   ws = map (encode index_table) paths
                 in unlines [
     "#include <stdint.h>",
     "#include \"path_config.h\"",
-    "uint32_t edgeIndexArr[EDGE_INDEX_LEN] = " ++ printEdges (getEdges (path graph)) ++ ";",
+    "uint32_t edgeIndexArr[EDGE_INDEX_LEN] = " ++ printEdges (nub $ concatMap snd tes) ++ ";",
     "uint32_t tableIndexArr[TABLE_INDEX_LEN] = " ++ printIndexTable index_table ++ ";",
     printf "uint32_t pathCodes[PATH_CODES_LEN] = " ++ printEncoding ws ++ ";"
     ]
@@ -66,10 +69,11 @@ table_info_cfile :: GQL -> String
 table_info_cfile graph = let paths = gql2Paths graph
                              p = path graph
                              tis = pathToTableInfo p
+                             tes = tableAdjacent p []
                          in unlines [
     "#include \"vertex_edges_info.h\"",
     "IdHashPair g_edge_id_hash_pairs[EDGE_IDS_SIZE] = " 
-        ++ (show $ (getEdges p)
+        ++ (show $ (nub $ concatMap snd tes)
                  & map (\(E e) -> indent 4 (braces ((pretty (drop 1 e)) <> pretty "," <+> (unsafeViaShow (hash32 e)))))
                  & concatWith (surround (comma <> line))
                  & \x -> line <> x
